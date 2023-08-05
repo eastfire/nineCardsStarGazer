f8a6db4e1ca18413cc824ed16fcd034e919ab752
eastfire <eastfire_he@hotmail.com>
2023-08-05 08:50:50 +0800

    initial

---
+++ src/components/QrcodeReader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <qrcode-stream :track="selected.value" @init="logErrors" />
+  </div>
+</template>
+
+<script setup>
+import { QrcodeStream } from "vue-qrcode-reader"
+
+
+</script>
+
+<style scoped>
+
+</style>
