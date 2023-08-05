479b69f3f6de83799c8278960d8836cf5a2b5f4e
eastfire <eastfire_he@hotmail.com>
2023-08-05 10:18:28 +0800

    qrcode-reader

--- src/components/QrcodeReader.vue
+++ src/components/QrcodeReader.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <qrcode-stream :track="selected.value" @init="logErrors" />
+    <qrcode-stream :track="paintOutline" @init="logErrors" @decode="onDecode" />
   </div>
 </template>
 
 <script setup>
 import { QrcodeStream } from "vue-qrcode-reader"
 
+const paintOutline = (detectedCodes, ctx) => {
+  for (const detectedCode of detectedCodes) {
+    const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
+
+    ctx.strokeStyle = 'red'
+
+    ctx.beginPath()
+    ctx.moveTo(firstPoint.x, firstPoint.y)
+    for (const { x, y } of otherPoints) {
+      ctx.lineTo(x, y)
+    }
+    ctx.lineTo(firstPoint.x, firstPoint.y)
+    ctx.closePath()
+    ctx.stroke()
+  }
+}
+
+const onDecode = (url) => {
+  console.log('get url', url)
+}
+
+function logErrors(promise) {
+  promise.catch(console.error)
+}
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
